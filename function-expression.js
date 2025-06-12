aaba22a4597e320bf7b33bb22205ee376d436074
Mo-create-git <msaffan078@gmail.com>
2025-06-12 22:44:43 +0530

    second commit

---
+++ function-expression.js
@@ -0,0 +1,7 @@
+//In javaScript, a function expression is a way to store function in variables.
+
+// store a function in the sqaure in the sqaure variable.
+let square = function(num){
+    return num * num; // returns the square of the number
+};
+console.log(square(5)); // Output: 25
